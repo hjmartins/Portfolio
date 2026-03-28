6a6d143b2ed4eaa81dbd0e55c3ea07d9468188d4
Henrique Martins <hjmartins88@gmail.com>
2026-03-28 19:02:30 -0300

    change button and slides

--- script.js
+++ script.js
@@ -1,29 +1,118 @@
-// VER MAIS
-function toggleMore(element) {
-  const moreText = element.parentElement.previousElementSibling;
+// INIT CAROUSEL
+document.querySelectorAll(".carousel").forEach(carousel => {
+  const images = carousel.querySelectorAll("img");
+  const dotsContainer = carousel.querySelector(".dots");
+  let index = 0;
+  let interval;
 
-  if (moreText.style.display === "block") {
-    moreText.style.display = "none";
+  // criar dots
+  images.forEach((_, i) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    if (i === 0) dot.classList.add("active");
+
+    dot.addEventListener("click", () => {
+      index = i;
+      update();
+    });
+
+    dotsContainer.appendChild(dot);
+  });
+
+  const dots = dotsContainer.querySelectorAll(".dot");
+
+  function update() {
+    images.forEach(img => img.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    images[index].classList.add("active");
+    dots[index].classList.add("active");
+  }
+
+  // NAV
+  carousel.querySelector(".next").onclick = () => {
+    index = (index + 1) % images.length;
+    update();
+  };
+
+  carousel.querySelector(".prev").onclick = () => {
+    index = (index - 1 + images.length) % images.length;
+    update();
+  };
+
+  // AUTOPLAY
+  function startAuto() {
+    interval = setInterval(() => {
+      index = (index + 1) % images.length;
+      update();
+    }, 3000);
+  }
+
+  function stopAuto() {
+    clearInterval(interval);
+  }
+
+  carousel.addEventListener("mouseenter", stopAuto);
+  carousel.addEventListener("mouseleave", startAuto);
+
+  startAuto();
+
+  // SWIPE MOBILE
+  let startX = 0;
+
+  carousel.addEventListener("touchstart", e => {
+    startX = e.touches[0].clientX;
+  });
+
+  carousel.addEventListener("touchend", e => {
+    let endX = e.changedTouches[0].clientX;
+
+    if (startX - endX > 50) {
+      index = (index + 1) % images.length;
+    } else if (endX - startX > 50) {
+      index = (index - 1 + images.length) % images.length;
+    }
+
+    update();
+  });
+
+  // MODAL
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+
+  images.forEach(img => {
+    img.onclick = () => {
+      modal.style.display = "block";
+      modalImg.src = img.src;
+    };
+  });
+
+  document.querySelector(".close").onclick = () => {
+    modal.style.display = "none";
+  };
+});
+
+// READ MORE
+function toggleMore(element) {
+  const more = element.parentElement.previousElementSibling;
+
+  if (more.style.display === "block") {
+    more.style.display = "none";
     element.innerText = "Read more";
   } else {
-    moreText.style.display = "block";
+    more.style.display = "block";
     element.innerText = "Show less";
   }
 }
 
-// ANIMAÇÃO AO SCROLL
+// SCROLL ANIMATION
 function revealOnScroll() {
-  const elements = document.querySelectorAll('.reveal');
-
-  elements.forEach(el => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - 50) {
-      el.classList.add('active');
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+      el.classList.add("active");
     }
   });
 }
 
-window.addEventListener('scroll', revealOnScroll);
+window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
